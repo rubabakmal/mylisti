64a9f84acce1353a19fb3d07208edd43108efca5
Rubab Akmal <rubabakmal19@gmail.com>
2024-04-19 16:17:29 +0500

    website design completed

--- js/main.js
+++ js/main.js
@@ -1,57 +1,69 @@
-document.querySelector(".right-bar").addEventListener("click", function () {
-  document.getElementById("myModal").style.display = "block";
-});
-document.querySelector(".close").addEventListener("click", function () {
-  document.getElementById("myModal").style.display = "none";
-});
-document.querySelector(".close").addEventListener("click", function () {
-  document.getElementById("myModal").style.display = "none";
-});
-document.getElementById("openNewModal").addEventListener("click", function () {
-  document.getElementById("myModal").style.display = "none";
-  document.getElementById("myModalNew").style.display = "block";
-});
-document.getElementById("closeModal").addEventListener("click", function () {
-  document.getElementById("myModal").style.display = "none";
-});
-document.getElementById("closeModalNew").addEventListener("click", function () {
-  document.getElementById("myModalNew").style.display = "none";
-});
-document.getElementById("closeModalNew").addEventListener("click", function () {
-  document.getElementById("myModalNew").style.display = "none";
-});
-document
-  .getElementById("openSecondModal")
-  .addEventListener("click", function () {
-    document.getElementById("myModalNew").style.display = "none";
-    document.getElementById("myModalSecond").style.display = "block";
-  });
-document
-  .getElementById("closeModalSecond")
-  .addEventListener("click", function () {
-    document.getElementById("myModalSecond").style.display = "none";
-  });
+{
+  /* // Get the modal */
+}
+var modal = document.getElementById("modalImage");
 
-$(document).ready(function () {
-  $(".add-to-cart-btn").click(function () {
-    var parentContainer = $(this).closest(".card-img-container");
-    $(this).hide();
-    var quantityText = parentContainer.find(".quantity-text");
-    quantityText.text("Qty 1"); // Reset quantity text to 1 when "Add to Cart" is clicked
-    parentContainer.find(".quantity").show();
-  });
-  $(".plus-btn").click(function () {
-    var parentContainer = $(this).closest(".card-img-container");
-    var quantityText = parentContainer.find(".quantity-text");
-    var currentValue = parseInt(quantityText.text().replace("Qty ", "") || 0);
-    quantityText.text("Qty " + (currentValue + 1));
-  });
-  $(".minus-btn").click(function () {
-    var parentContainer = $(this).closest(".card-img-container");
-    var quantityText = parentContainer.find(".quantity-text");
-    var currentValue = parseInt(quantityText.text().replace("Qty ", "") || 0);
-    if (currentValue > 1) {
-      quantityText.text("Qty " + (currentValue - 1));
-    }
-  });
-});
+{
+  /* // Get the button that opens the modal */
+}
+var btn = document.getElementById("openModalImageBtn");
+
+{
+  /* // Get the <span> element that closes the modal */
+}
+var span = document.getElementsByClassName("my-close")[0];
+
+{
+  /* // When the user clicks the button, open the modal */
+}
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+{
+  /* <!-- Add your custom modal script here --> */
+}
+{
+  /* // Get the modal */
+}
+var customModal = document.getElementById("customModal");
+
+// Get the button that opens the modal
+var customBtn = document.getElementById("openCustomModalBtn");
+
+{
+  /* // Get the <span> element that closes the modal */
+}
+var customSpan = document.getElementsByClassName("custom-close")[0];
+
+{
+  /* // When the user clicks the button, open the modal */
+}
+customBtn.onclick = function (event) {
+  event.preventDefault(); // Prevent the default action
+  customModal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+customSpan.onclick = function () {
+  customModal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == customModal) {
+    customModal.style.display = "none";
+  }
+};
